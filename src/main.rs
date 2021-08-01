4be1788a92cbc11b6b366d6ccc6f658f92027a6d
mayone-du <cocomayo1201@icloud.com>
2021-08-01 23:03:26 +0900

    Add rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("数あてゲームスタート！");
+    let secret_number: i32 = rand::thread_rng().gen();
+
+    println!("予想を入力してください！");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("エラーが発生しました。");
+
+    println!("あなたが予想した数: {}", guess);
 }
